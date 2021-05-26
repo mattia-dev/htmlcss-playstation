b0a3b95b34fafe07d699fbdf63f42564c6367702
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-05-26 15:47:58 +0200

    added js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+alert('Premi Ok per visualizzare la pagina');
+
+document.getElementById('test-js').innerHTML ='PlayStation: For The Players';
+
+var x = 10;
+var y = 15;
+var z = x + y;
+
+console.log(z);
